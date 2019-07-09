1ec891c75b137c5d8a53c35ab4ed1e2dd28588a2
Caio Campos <campos.f.caio@gmail.com>
2019-07-09 19:36:04 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/caiocampos/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.caiocampos.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiocampos.cursomc.domain.Categoria;
+import com.caiocampos.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+
+}
