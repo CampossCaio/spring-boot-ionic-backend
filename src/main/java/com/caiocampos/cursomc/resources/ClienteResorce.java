93734469ae9d687cbb257f1b5210986c86f60e18
Caio Campos <campos.f.caio@gmail.com>
2019-07-15 19:56:17 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/caiocampos/cursomc/resources/ClienteResorce.java
@@ -0,0 +1,25 @@
+package com.caiocampos.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caiocampos.cursomc.domain.Cliente;
+import com.caiocampos.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResorce {
+	
+	@Autowired
+	ClienteService service;
+	
+	@RequestMapping(value ="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?>find(@PathVariable Integer id){
+	Cliente obj = service.buscar(id);
+	return ResponseEntity.ok(obj);
+	}
+}
