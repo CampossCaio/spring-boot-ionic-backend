9fb55cff93ef7da2a72a9b7b472c560e1cd88455
Caio Campos <campos.f.caio@gmail.com>
2019-07-30 19:16:45 -0300

    Validacao customizada: email não repetido na atualizacao de clientes

--- src/main/java/com/caiocampos/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/caiocampos/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.caiocampos.cursomc.domain.Cliente;
+import com.caiocampos.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
