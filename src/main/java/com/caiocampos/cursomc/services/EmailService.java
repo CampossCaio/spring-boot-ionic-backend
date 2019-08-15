4e9f75294afb436529223af08333aab8296f2260
Caio Campos <campos.f.caio@gmail.com>
2019-08-14 22:16:29 -0300

    Email HTML

--- src/main/java/com/caiocampos/cursomc/services/EmailService.java
+++ src/main/java/com/caiocampos/cursomc/services/EmailService.java
@@ -1,14 +1,22 @@
 package com.caiocampos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.caiocampos.cursomc.domain.Pedido;
 
 public interface EmailService {
 	
+	//Email versão texto
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	// Email Versão Html
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 		
 
 }
