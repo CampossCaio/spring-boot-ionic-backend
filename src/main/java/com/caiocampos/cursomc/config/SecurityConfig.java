2009cd594de3d51519dd7916f1d15eca9f85d47a
Caio Campos <campos.f.caio@gmail.com>
2019-08-27 00:41:09 -0300

    Implementando autorização

--- src/main/java/com/caiocampos/cursomc/config/SecurityConfig.java
+++ src/main/java/com/caiocampos/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.caiocampos.cursomc.security.JWTAuthenticationFilter;
+import com.caiocampos.cursomc.security.JWTAutorizationFilter;
 import com.caiocampos.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAutorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
