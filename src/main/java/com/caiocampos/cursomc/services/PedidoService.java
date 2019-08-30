adc7a3608c3b370019ada5c776b343c3c0ac546e
Caio Campos <campos.f.caio@gmail.com>
2019-08-29 23:51:10 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/caiocampos/cursomc/services/PedidoService.java
+++ src/main/java/com/caiocampos/cursomc/services/PedidoService.java
@@ -3,9 +3,13 @@ package com.caiocampos.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.caiocampos.cursomc.domain.Cliente;
 import com.caiocampos.cursomc.domain.ItemPedido;
 import com.caiocampos.cursomc.domain.PagamentoComBoleto;
 import com.caiocampos.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.caiocampos.cursomc.repositories.ItemPedidoRepository;
 import com.caiocampos.cursomc.repositories.PagamentoRepository;
 import com.caiocampos.cursomc.repositories.PedidoRepository;
 import com.caiocampos.cursomc.repositories.ProdutoRepository;
+import com.caiocampos.cursomc.security.UserSS;
+import com.caiocampos.cursomc.services.exceptions.AuthorizationException;
 import com.caiocampos.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,5 +78,15 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
-
+	
+	public Page<Pedido>findPage(Integer page,Integer linesPerPage,String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
